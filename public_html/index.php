00b3681725874c3abb45a0d9c582c5a913db3148
Paul Park <camo1018@yahoo.com>
2013-10-19 05:40:46 -0700

    Changed PHP Scripts

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,8 @@
 <?php
-	include 'php/core/init.php';
-?>	
-
+   include 'php/core/init.php';
+?>
+<!DOCTYPE html>
 <html>
-
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/global/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
