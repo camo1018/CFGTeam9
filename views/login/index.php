ad375a0893079e8579626227eff470a6ef9b6467
Michael Lu <lumichael94@gmail.com>
2013-10-18 23:14:55 -0400

    Message

---
+++ views/login/index.php
@@ -0,0 +1,17 @@
+<html>
+<head><link href ="../resources/library/css/bootstrap.min.css" rel=stylesheet" media = "screen">
+<link href="../../resources/library/css/styles.css" rel="stylesheet">
+<link href="../../resources/library/css/Login/index.css" rel="stylesheet">
+</head>
+<body>
+	<form id="login_form" action = 'login.php' method='post'accept-charset='UTF-8'>
+			<fieldset>
+				<input type='text' placeholder= "username" name='username' id='username'  maxlength="50" />
+				
+				<input type='password' placeholder= "password" name='password' id='password' maxlength="50" />
+				 
+				<input type='submit' name='Submit' value='Submit' />
+			</fieldset>
+	</form>
+</body>
+</html>
