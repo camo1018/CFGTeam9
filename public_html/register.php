d748c5aaf289fcac6aa2e48b9e57167ce11d1ec0
Michael Lu <lumichael94@gmail.com>
2013-10-19 01:48:33 -0400

    Login and Register Page

--- public_html/register.php
+++ public_html/register.php
@@ -2,20 +2,19 @@
 <html>
 <head><link href ="css/global/bootstrap/bootstrap.min.css" rel="stylesheet" media = "screen">
 <link href="css/global/styles.css" rel="stylesheet">
-<link href="css/login/login.css" rel="stylesheet">
+<link href="css/register/register.css" rel="stylesheet">
 </head>
 <body>
-<h1>Login</h1>
-<div id="signin-container">
+<h1>Register</h1>
+<div id="register-container">
 
-	<form id="login_form" name ="login_form" action = 'login.php' class= "form-signin" method='post'accept-charset='UTF-8'>
+	<form id="register_form" name ="register_form" action = 'register.php' class= "form-register" method='post'accept-charset='UTF-8'>
 			<fieldset>
 				<input type='text' class= "form-control"  placeholder= "username" name='username' id='username'  maxlength="50" />
 				
 				<input type='password' class= "form-control" placeholder= "password" name='password' id='password' maxlength="50" />
 				 
 				<input type='submit' id="submit-button" class= "btn btn-primary" name='Submit' value='Submit' />
-				<input type="checkbox" name="keeplog" value="keeplog">Keep Logged In?
 			</fieldset>
 	</form>
 	</div>
