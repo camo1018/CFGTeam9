ac0d9f826d2d6dce07091f28a670a55c4662742d
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2013-10-19 10:56:44 -0400

    adding changes to about ui

--- public_html/about.php
+++ public_html/about.php
@@ -30,25 +30,16 @@
 			</div>
 		</div>
 	</div>
-	<div id = "about-container">
-	<div id = "pic-container">
-		<div id= "about-img" >
-                        <img src="assets/img/about-pic.jpg" width="250" alt="about-img">
-		</div>
-		<div id= "about-img2">
-			<img src="assets/img/about-pic2.jpg" width="250" alt-"about-img2">
-		</div>
-		<div id="about-img3">
-			<img src="assets/img/about-pic3.jpg" width="250" alt-"about-img3">
-		</div>
-	</div>
-	<div id = "faq-container" style="width 500;">
-		<div id= "general-about">
-			<img src="assets/img/manafitLogo.png" width="400" alt="about-logo">
-			<p style="font-style: italic; line-height:300%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+	
+	<div class="container">
+	<div class="row">
+		<div class="col-md-8 pull-right">
+			<div id= "general-about">
+				<img src="assets/img/manafitLogo.png" width="400" alt="about-logo">
+				<p style="font-style: italic; line-height:300%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>	
+			</div>
 			
-		</div>
-		<div id= "faq-about" style= "width: 500px;">
+
 			<h2>Frequently Asked Questions</h2>
 			<div id="leftbar">
 				<h3>What is Mana Fitness?</h3>
@@ -67,9 +58,23 @@
 				<p>Nulla id dui bibendum, tincidunt velit a, posuere nisi. Aliquam erat volutpat. Fusce dignissim ipsum vel magna feugiat, at tincidunt dolor convallis. </p>
 			</div>
 		</div>
-	<div class="clear"></div>
+		
+		<div class="col-md-4 pull-left">
+			<div class="row" style="padding:20px;">
+				<img src="assets/img/about-pic.jpg" width="250" alt="about-img"/>
+			</div>
+			<div class="row" style="padding:20px;">
+				<img src="assets/img/about-pic2.jpg" width="250" alt="about-img2"/>
+			</div>
+			<div class="row" style="padding:20px;">
+				<img src="assets/img/about-pic3.jpg" width="250" alt="about-img3"/>
+			</div>	
+		</div>
+		
+		
+		
 	</div>
-	
+	</div>
 	
 	<script src="js/global/bootstrap/jquery.js"></script>
     <script src="js/global/bootstrap/bootstrap.min.js"></script>
